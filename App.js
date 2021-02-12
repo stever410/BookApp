d4f867fb084fd2e04f27ce922297dd91cf455a16
stever410@gmail.com <ngotanduc410@gmail.com>
2021-02-12 21:05:06 +0700

    Init project

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import Header from './src/components/Header';
+
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
