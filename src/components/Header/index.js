675be96afda0f016a5ae261a188a372925a87675
stever410@gmail.com <ngotanduc410@gmail.com>
2021-02-12 21:04:52 +0700

    Init project

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const Header = () => {
+  return (
+    <>
+      <View>
+        <Icon></Icon>
+      </View>
+    </>
+  );
+};
+
+export default Header;
